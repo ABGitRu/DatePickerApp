54ed0e1cdb44c8dced913300be72fd412ede5667
Alexey Barinov <begoodbebad2012@gmail.com>
2021-03-31 21:03:03 +0300

    Added: DatePicker TimerPicker Timer

--- DatePickerApp/ViewController.swift
+++ DatePickerApp/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var datelabel: UILabel!
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBOutlet var timerPicker: UIDatePicker!
+    
+    @IBOutlet var fireTimer: UIButton!
+    
+    var counter = 0
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.calendar = .autoupdatingCurrent
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        timerPicker.locale = Locale(identifier: "ru_RU")
+        
+        fireTimer.layer.cornerRadius = 10
     }
 
+    @IBAction func dateChanged(_ sender: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .full
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        
+        let date = dateFormatter.string(from: sender.date)
+        
+        datelabel.text = date
+    }
+    
+    @IBAction func fireTimerButtonTapped() {
+        counter = Int(timerPicker.countDownDuration)
+        timerPicker.countDownDuration = 0
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.counter -= 1
+            self.timerLabel.text =  "Осталось \(self.counter) секунд"
+            
+            if self.counter == 0 {
+                timer.invalidate()
+            }
+        }
+    }
 
+    
 }
 
